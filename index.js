ef7de985b569f53abaf87d0c782a75bb88c8aac1
Zohre-MZ <zohr5882@gmail.com>
2025-04-09 21:50:48 +0400

    commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("hello world");
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+// Function to create the browser window
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,  // Allows renderer process (HTML, JS) to access Node.js APIs
+      contextIsolation: false // Allow using Node.js features directly
+    }
+  });
+
+  // Load the index.html of the app
+  win.loadFile('index.html');
+
+  // Open DevTools (optional)
+  win.webContents.openDevTools();
+}
+
+// When Electron has finished initialization, create the window
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Quit the app when all windows are closed
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
